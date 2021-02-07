9d2dc8b8f5ad568d1867cf4283ce7422f3f04c77
T22007 <78694408+T22007@users.noreply.github.com>
2021-02-07 19:21:27 +0300

    TLTL code

---
+++ TLTL.py
@@ -0,0 +1,81 @@
+print ("kosh amadid")
+print ("mammad: bia berim sar koche noshabe bekharim miai?")
+s1 = input ("1-are \n2-na \n")
+if s1 == "1":
+    print ("-ok bia berim")
+    print ("...")
+    print ("-khob residim zard ya meshki?")
+    s1_2 = input ("1-zard \n2-meshki\n")
+    if s1_2 == "1" :
+        print ("-man ke ziad khosham namiad vali bashe bia bargardim")
+        print ("...")
+        print ("armin: salam bache ha")
+        print ("mammad: salam armin")
+        s1_3 = input ("1-salam che khabar?\n")
+        if  s1_3 == "1" :
+                    print ("armin: amir omad dam dar karet dasht nabody")
+                    s1_4 = input ("1-ei baba \n2-lanati \n")
+                    if s1_4 == "1" :
+                            print ("(payan)")
+                    elif s1_4 == "2":
+                            print ("(tamom shod)")
+    elif s1_2 == "2" :
+        print ("khobe bia bargardim")
+        print ("...")
+        print ("armin: salam bache ha")
+        print ("mammad: salam armin")
+        s1_3 = input ("1-salam che khabar?\n")
+        if  s1_3 == "1" :
+                    print ("armin: amir omad dam dar karet dasht nabody")
+                    s1_4 = input ("1-ei baba \n2-lanati \n")
+                    if s1_4 == "1" :
+                            print ("payan")
+                    elif s1_4 == "2" :
+                            print ("tamom shod")
+
+
+
+
+
+elif s1 == "2" :
+    print ("bashe tanha miram")
+    print ("...")
+    print ("biiiing")
+    s2_1 = input("1-kie\n")
+    if s2_1 == "1" :
+        print ("-manam amir")
+        s2_2 = input ("1-befarma too \n2-kari ba man dari?\n")
+        if s2_2 == "1":
+            print("na mamnon ye kari bahat dashtam")
+            s2_3 = input("1-che kari?\n2")
+            if s2_3 == "1":
+                    print ("in cd oon baziast ke azash khoshet omad barat avordamesh")
+                    s2_4 = input ("1-bashe bedesh be man \n2-na namikhamesh \n")
+                    if s2_4 == "1":
+                        print ("ok bia")
+                        print ("payan")
+                    elif s2_4 == "2":
+                        print ("har joor rahati")
+                        print ("payan")
+        elif s2_2 == "2":
+                print ("are")
+                print ("in cd oon baziast ke azash khoshet omad barat avordamesh")
+                s2_4 = input ("1-bashe bedesh be man \n2-na namikhamesh \n")
+                if s2_4 == "1":
+                        print ("ok bia")
+                        print ("payan")
+                elif s2_4 == "2":
+                        print ("har joor rahati")
+                        print ("payan")
+            
+        
+                
+            
+    
+          
+        
+        
+        
+    
+
+    
